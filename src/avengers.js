3ee684043fd8a7acd00c771de724340dec331845
Adam Hinckley <adamhinckley@Adams-MacBook-Pro.local>
2018-10-05 17:34:46 -0600

    first commit

---
+++ src/avengers.js
@@ -0,0 +1,12 @@
+const avengers = [
+  {
+    name: "Captain America",
+    power: "Who knows, but he has a cool shield"
+  },
+  {
+    name: "Hulk",
+    power: "Hulk SMASH!"
+  }
+];
+
+export default avengers;
